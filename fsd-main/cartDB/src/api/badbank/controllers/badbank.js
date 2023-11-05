9abc9aff2e66a9c3568207448df3c606e15df878
sundarapandiyan <111949963+Sundarapandiyan504@users.noreply.github.com>
2023-11-06 01:24:52 +0530

    Add files via upload

---
+++ fsd-main/cartDB/src/api/badbank/controllers/badbank.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * badbank controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::badbank.badbank');
---
+++ fsd-main/cartDB/src/api/badbank/routes/badbank.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * badbank router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::badbank.badbank');
